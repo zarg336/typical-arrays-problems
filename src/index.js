9add3cba03e3bb34b227ac95cefe1a60d2ddf6c7
zarg336 <70839905+zarg336@users.noreply.github.com>
2020-10-01 22:26:40 +0300

    feat: add function min, max, avg.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
+  exports.min = function min(array) {
+      if (array === undefined || array.length === 0) return 0;
+      let minimal = array[0];
+      array.forEach((i) => {
+          if (i < minimal) return minimal = i;
+      });
+      return minimal;
+  }
 
-exports.min = function min (array) {
-  return 0;
-}
+  exports.max = function max(array) {
+      if (array === undefined || array.length === 0) return 0;
+      let maximum = 0;
+      array.forEach((i) => {
+          if (i > maximum) return maximum = i;
+      });
+      return maximum;
+  }
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+  exports.avg = function avg(array) {
+      if (array === undefined || array.length === 0) return 0;
+      const i = (accumulator, value) => accumulator + value;
+      return (array.reduce(i) / array.length);
+  }
